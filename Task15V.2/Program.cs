5d129baae312400c2bb6d1f9fb5c92d12c0c1b98
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-06-27 22:27:50 +0300

    решение задачи 15 с исползованием массива

--- Task15V.2/Program.cs
+++ Task15V.2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите день недели (от 1 до 7)");
+
+string[] array = {" ", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Выходной", "Выходной"};
+int n = array.Length;
+int index = Convert.ToInt32(Console.ReadLine());
+
+while (index < n && index > 0)
+{
+    Console.WriteLine(array[index]);
+    break;
+}
+if (index <1 ^ index >7)
+{
+    Console.WriteLine("Нет такого дня недели");
+}
