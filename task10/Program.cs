3ad5866b63acc3b3483530645679e0330532e36d
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-06-27 21:22:10 +0300

    Task10 is done

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на 
+//вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void FindSecondNumber(int n1)
+{
+    int result = (n1 - (n1/100)*100)/10;
+    Console.WriteLine("Second number is " + result);
+
+}
+
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+FindSecondNumber(number);
