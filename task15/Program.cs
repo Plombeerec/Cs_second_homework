feb4a028061338121506c7af55d3613469e71651
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-06-27 21:52:59 +0300

    task 15 var1 is done

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void CheckDay(int day)
+{
+     if (day >5 && day <7)
+     {
+        Console.WriteLine("Это выходной день");
+     }
+     else if (day == 1)
+     {
+        Console.WriteLine("Понедельник");
+     }
+     else if (day == 2)
+     {
+        Console.WriteLine("Вторник");
+     }else if (day == 1)
+     {
+        Console.WriteLine("Понедельник");
+     }else if (day == 3)
+     {
+        Console.WriteLine("Среда");
+     }else if (day == 4)
+     {
+        Console.WriteLine("Четверг");
+     }
+     else if (day == 5)
+     {
+        Console.WriteLine("Пятница");
+     }
+     else
+     {
+        Console.WriteLine("НЕТ такого дня недели!");
+     }
+}
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+CheckDay(number);
+
