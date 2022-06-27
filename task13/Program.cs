d90ccf3d3f313ef703551e785717d199b729ae84
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-06-27 21:27:51 +0300

    Task13 is done

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, 
+//которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void CheckThird (int n)
+{
+    if (n/100 == 0)
+    {
+        Console.WriteLine("Третей цифры в числе нет");
+    }
+    else
+    {
+        int result = n/100 ;
+        Console.WriteLine("Second number is " + result);
+    }
+    
+}
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+CheckThird(number);
